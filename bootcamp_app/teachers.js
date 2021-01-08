803d282368830617d4f9e9df5adcc56c3d9b00ff
zahra-hm <zahra520@hotmail.com>
2021-01-08 03:59:50 +0000

    sql injections

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,3 +1,4 @@
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -7,17 +8,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
-JOIN students ON student_id = students.id
-JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
-.then(res => {
+const cohortName = process.argv[2] || "JUL02";
+const values = [cohortName];
+const queryString = `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+    FROM teachers
+      JOIN assistance_requests ON teachers.id = teacher_id
+      JOIN students ON students.id = student_id
+      JOIN cohorts ON cohorts.id = cohort_id
+    WHERE cohorts.name = $1
+    ORDER BY teacher;
+`;
+pool.query(queryString, values).then(res => {
   res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
-  })
-}); 
+    console.log(`${row.cohort}; ${row.teacher}`);
+  });
+});
